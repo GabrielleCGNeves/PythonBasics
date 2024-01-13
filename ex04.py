6a741cb11f51f9650f1d198c26314e64d355fc67
GabrielleCGNeves <88461057+GabrielleCGNeves@users.noreply.github.com>
2024-01-13 16:18:43 +0000

    :sparkles: operations exercise

---
+++ ex04.py
@@ -0,0 +1,21 @@
+# EXERCÍCIO 4. Operadores
+
+# x = 2 + 3
+# if x == 5:
+#   print(x)
+# else:
+#   print("X não é igual a 5.")
+
+
+# x = 10
+# x |= 10
+# print(x)
+
+x = True
+y = True
+
+if (x == True) | (y == True):
+  print("É verdadeiro")
+else:
+  print("É falso")
+  
