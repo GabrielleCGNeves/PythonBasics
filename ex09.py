f9bae40035379fcf69a6c3f1fb29263dcd90701c
GabrielleCGNeves <88461057+GabrielleCGNeves@users.noreply.github.com>
2024-01-13 18:46:48 +0000

    :sparkles: function exercise

---
+++ ex09.py
@@ -0,0 +1,12 @@
+# Descreva como você faria e mostre esse código
+
+# O algoritmo abaixo implementa uma função chamada exercício, cujo valor 1 é
+# a variável chamada num. Levando em consideração os conceitos de variável
+# local e global, marque a alternativa correta
+
+num = 0
+def exercício():
+    num= 1
+    print(num)
+exercício()
+print(num)
